e658b19881c6beff40ebd1b3fbf23fdc17a2f334
t1okumikatu <t1okumikatu@gmail.com>
2026-05-19 15:31:54 +0900

    up

--- RTCとjoy_pub/run_rtc.sh
+++ RTCとjoy_pub/run_rtc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 1. 現在のディレクトリに移動するように変更
-cd "$(dirname "$0")"
-
+# 1. RTCとjoy_pub ディレクトリに移動
+cd "$(dirname "$0")/RTCとjoy_pub"
 # 2. 実行権限を付与して起動
 chmod +x ./go2rtc_linux_amd64
 ./go2rtc_linux_amd64
